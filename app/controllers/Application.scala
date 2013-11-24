0f93ee704178acf519eca1f4a22c5b91f0c468bc
George Lewis <george@lendup.com>
2013-11-23 20:03:16 -0800

    Adds tweet helper methods

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,7 +7,7 @@ import helpers.TwitterApi
 import scala.collection.JavaConverters._
 
 
-import models.Tweets
+import models.{Tweet, Tweets}
 import org.json4s._
 import org.json4s.native.JsonMethods
 
@@ -34,13 +34,15 @@ object Application extends Controller {
   }
 
 
-  def addCoffee = Action {
-
+  def addTweet = Action {
     val myVal = JsonMethods.parse(""" { "numbers" : [1, 2, 3, 4] } """)
+    val id: Long = Tweets.add(myVal, LocalDateTime.now())
+    Ok(views.html.index("Created Tweet: $id"))
+  }
 
-    Tweets.add(myVal, LocalDateTime.now())
-
-    Ok(views.html.index("Your new application is ready."))
+  def getTweet(id: Long) = Action {
+    val tweet = Tweets.fetch(id).getStatus
+    Ok(views.html.index("Created Tweet: %s".format(tweet.toString)))
   }
 
 
--- app/models/Tweets.scala
+++ app/models/Tweets.scala
@@ -3,18 +3,47 @@ package models
 import org.joda.time.LocalDateTime
 
 import app.MyPostgresDriver.simple._
-import org.json4s.JValue
+import org.json4s.{jsonwritable, JValue}
+import org.json4s.native.Serialization.write
+
+import twitter4j.Status
 
 import scala.slick.lifted._
 
 import play.api.Play.current
+
+import play.Logger
 
 
 // SEE: https://github.com/ThomasAlexandre/slickcrudsample/
 // http://java.dzone.com/articles/getting-started-play-21-scala
 
 // TODO: Convert to DateTime
-case class Tweet(id: Option[Long], content: JValue, fetchedAt: LocalDateTime)
+case class Tweet(id: Option[Long], content: JValue, fetchedAt: LocalDateTime) {
+
+  // Convert the internal json4s object to a string
+  def jsonString(): String = {
+    implicit val formats = org.json4s.DefaultFormats
+    return  write(content) //content.extract[String]
+  }
+
+  // Convert the internal json4s object to a
+  // twitter4j Status object
+  def getStatus: twitter4j.Status = {
+    try {
+
+      val testString: String = "{createdAt=2013-11-20, id=404331790371807232, text='@remindtweets This looks interesting', source='web', isTruncated=false, inReplyToStatusId=-1, inReplyToUserId=2205796142, isFavorited=false, inReplyToScreenName='remindtweets', geoLocation=null, place=null, retweetCount=0, isPossiblySensitive=false, contributorsIDs=J@a49c723, retweetedStatus=null, userMentionEntities=[{name='George', screenName='remindtweets', id=2205796142}], urlEntities=[], hashtagEntities=[], mediaEntities=[], currentUserRetweetId=-1, user={id=42805000, name='Herbie Lewis', screenName='HerbieLewis', location='New York', description='Ending the world one proton at a time...', isContributorsEnabled=false, profileImageUrl='http://pbs.twimg.com/profile_images/1333372815/MeBeer_normal.jpg', profileImageUrlHttps='https://pbs.twimg.com/profile_images/1333372815/MeBeer_normal.jpg', url='http://t.co/Wp8W6tZCbf', isProtected=false, followersCount=123, status=null, profileBackgroundColor='C0DEED', profileTextColor='333333', profileLinkColor='0084B4', profileSidebarFillColor='DDEEF6', profileSidebarBorderColor='C0DEED', profileUseBackgroundImage=true, showAllInlineMedia=false, friendsCount=436, createdAt=2011-11-23, favouritesCount=12, utcOffset=-21600, timeZone='Central Time (US & Canada)', profileBackgroundImageUrl='http://abs.twimg.com/images/themes/theme1/bg.png', profileBackgroundImageUrlHttps='https://abs.twimg.com/images/themes/theme1/bg.png', profileBackgroundTiled=false, lang='en', statusesCount=1530, isGeoEnabled=false, isVerified=false, translator=false, listedCount=2, isFollowRequestSent=false}}"
+      return twitter4j.json.DataObjectFactory.createStatus(testString)
+    }
+    catch {
+      case e: Exception =>
+        Logger.error("Failed to create twitter status", e)
+        return null
+    }
+    return null
+  }
+
+}
 
 // Definition of the COFFEES table
 object Tweets extends Table[Tweet]("tweet") {
@@ -25,6 +54,7 @@ object Tweets extends Table[Tweet]("tweet") {
 
   def * : ColumnBase[Tweet] = (id.? ~ content ~ fetchedAt) <> (Tweet .apply _, Tweet.unapply _)
 
+  // These are both necessary for auto increment to work with psql
   def autoInc = content ~ fetchedAt returning id
 
   def add(json: JValue, fetchedAt: LocalDateTime): Long = {
@@ -33,6 +63,12 @@ object Tweets extends Table[Tweet]("tweet") {
     }
   }
 
+  def fetch(id: Long): Tweet = {
+    play.api.db.slick.DB.withSession{implicit session: Session =>
+      (for { b <- Tweets if b.id is id} yield b).first
+    }
+  }
+
   // Create a new instance
-  def create(content: JValue, fetchedAt: LocalDateTime=LocalDateTime.now()) = Tweet(Option(15L), content, fetchedAt)
+  //def create(content: JValue, fetchedAt: LocalDateTime=LocalDateTime.now()) = Tweet(Option(15L), content, fetchedAt)
 }
