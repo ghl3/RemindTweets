f73c1355c278d25d759bceb68999bd60bffbb651
George Lewis <ghl227@gmail.com>
2014-08-16 21:53:50 -0700

    Improve automatic testing

--- test/TestMentionParsing.scala
+++ test/TestMentionParsing.scala
@@ -7,6 +7,29 @@ import org.junit.Test
 
 class TestMentionParsing extends JUnitSuite {
 
+
+  def assertParsedProperly(mention: String, expectedValues: Map[String,String], notPresentKeys: List[String]): Unit = {
+
+    val parsed = ReminderParsing.parseStatusTextIntoReminderData(mention)
+    assert(parsed.isDefined)
+
+    parsed match {
+      case Some(data) =>
+        for ((key, value) <- expectedValues) {
+          assert(data.contains(key) === true)
+          assert(data(key) === value)
+        }
+        for (key <- notPresentKeys) {
+          assert(data.contains(key) === false)
+        }
+
+      case _ => assert(false, "Reminder not parsed")
+    }
+  }
+
+  def assertParsedProperly(mention: String, expectedValues: Map[String,String]): Unit = assertParsedProperly(mention, expectedValues, List())
+
+
   @Test
   def timeOfdayParsing() {
 
@@ -19,7 +42,6 @@ class TestMentionParsing extends JUnitSuite {
     timeOfday = ReminderParsing.parseTwelveHour("5:00PM")
     assert(timeOfday.isDefined)
     assert(timeOfday.get == new LocalTime(17,0,0))
-
   }
 
 
@@ -35,89 +57,69 @@ class TestMentionParsing extends JUnitSuite {
       .withSecondOfMinute(0).withMillisOfSecond(0)
 
     assert(todayAtFive === reminderTime.get)
-
   }
 
 
   @Test def structuredReminderA() {
     val mention = "@remindtweets Remind me to build this app on Wednesday at 5:00PM every week"
-    val structured = ReminderParsing.parseStatusTextIntoReminderData(mention).get
 
-    assert(structured("what") == "build this app")
-    assert(structured("every") == "every week")
-    assert(structured("to") == "to")
-    assert(structured("at") == "at 5:00PM")
-    assert(structured("on") == "on Wednesday")
-    assert(structured("time") == "5:00PM")
-    assert(structured("repeat") == "week")
-    assert(structured("when") == "Wednesday")
-  }
+    val data = Map(
+      "what"->"build this app",
+      "every"->"every week",
+      "to"->"to",
+      "at"->"at 5:00PM",
+      "on"->"on Wednesday",
+      "time"->"5:00PM",
+      "repeat"->"week",
+      "when"->"Wednesday")
 
-
-
-  def assertParsedProperly(mention: String, expectedValues: Map[String,String]) = {
-
-    val parsed = ReminderParsing.parseStatusTextIntoReminderData(mention)
-    assert(parsed.isDefined)
-
-    parsed match {
-      case Some(data) =>
-        for ((key, value) <- expectedValues) {
-          assert(data.contains(key) === true)
-          assert(data.get(key) === value)
-        }
-      case _ => assert(false, "Reminder not parsed")
-    }
+    assertParsedProperly(mention, data)
   }
 
 
   @Test def parsingA() {
     val mention = "@RemindTweets Remind me to eat lunch in 4 hours"
-    val parsed = ReminderParsing.parseStatusTextIntoReminderData(mention)
-    assert(parsed.isDefined)
 
-    parsed match {
-      case Some(data) =>
-
-        assert(!data.contains("repeat"))
-        assert(data("what") === "eat lunch")
-        assert(data("relativeTime") === "4 hours")
-
-      case _ => assert(false)
-    }
+    val data = Map("what"->"eat lunch", "relativeTime"->"4 hours")
+    val missingKeys = List("repeat")
+    assertParsedProperly(mention, data, missingKeys)
   }
 
 
   @Test def parsingB() {
     val mention = "@RemindTweets Remind me to get dinner at 5:40pm."
-    val parsed = ReminderParsing.parseStatusTextIntoReminderData(mention)
-    assert(parsed.isDefined, "Didn't parse successfully")
 
-    parsed match {
-      case Some(data) =>
-
-        assert(!data.contains("repeat"))
-        assert(data("what") === "get dinner")
-        assert(data("time") === "5:40pm")
-
-      case _ => assert(false)
-    }
+    val data = Map("what"->"get dinner", "time"->"5:40pm")
+    val missingKeys = List("repeat")
+    assertParsedProperly(mention, data, missingKeys)
   }
 
 
   @Test def parsingC() {
     val mention = " @remindtweets Remind me to get chips at the store at 6:15pm."
-    val parsed = ReminderParsing.parseStatusTextIntoReminderData(mention)
-    assert(parsed.isDefined, "Didn't parse successfully")
 
-    parsed match {
-      case Some(data) =>
-
-        assert(!data.contains("repeat"))
-        assert(data("what") === "get chips at the store")
-        assert(data("time") === "6:15pm")
-
-      case _ => assert(false)
-    }
+    val data = Map("what"->"get chips at the store", "time"->"6:15pm")
+    val missingKeys = List("repeat")
+    assertParsedProperly(mention, data, missingKeys)
   }
+
+
+  @Test def parsingD() {
+
+    val mention = "@remindtweets Remind me to build this app on Tomorrow at 6:00 PM"
+
+    val data = Map("what"->"build this app", "time"->"6:00 PM", "when"->"Tomorrow")
+    val missingKeys = List("repeat")
+    assertParsedProperly(mention, data, missingKeys)
+  }
+
+
+  @Test def parsingE() {
+
+    val mention = "@remindtweets Remind me to build this app on Wednesday at 6:00PM every week"
+
+    val data = Map("what"->"build this app", "time"->"6:00PM", "when"->"Wednesday", "repeat"->"week")
+    assertParsedProperly(mention, data)
+  }
+
 }
